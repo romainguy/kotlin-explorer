3e55e7af5aee32f65385f31cda3daea0c355a513
Omico <me@omico.icu>
2024-03-23 13:11:55 -0700

    Support shortcut keys on non-macOS (#11)

--- src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/OS.kt
+++ src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/OS.kt
@@ -17,3 +17,4 @@
 package dev.romainguy.kotlin.explorer
 
 val isWindows: Boolean = System.getProperty("os.name").lowercase().startsWith("win")
+val isMac: Boolean = System.getProperty("os.name").lowercase().startsWith("mac")
