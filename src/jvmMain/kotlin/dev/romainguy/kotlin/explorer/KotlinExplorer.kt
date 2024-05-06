5961cefa25f5674c974e25d2fc99a34ae022c4d4
Romain Guy <romainguy@curious-creature.com>
2024-05-06 08:56:45 -0700

    Add a Progress Indicator (#35)
    * Add a Progress Indicator
    
    * Fix dependencies
    
    Co-authored-by: Alon Albert <aalbert@google.com>

--- src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/KotlinExplorer.kt
+++ src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/KotlinExplorer.kt
@@ -20,8 +20,10 @@ package dev.romainguy.kotlin.explorer
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.input.key.Key.Companion.D
@@ -76,6 +78,7 @@ private fun FrameWindowScope.KotlinExplorer(
     // TODO: Move all those remembers to an internal private state object
     var activeTextArea by remember { mutableStateOf<RSyntaxTextArea?>(null) }
     var status by remember { mutableStateOf("Ready") }
+    var progress by remember { mutableStateOf(1f) }
 
     val searchListener = remember { object : SearchListener {
         override fun searchEvent(e: SearchEvent?) {
@@ -125,7 +128,11 @@ private fun FrameWindowScope.KotlinExplorer(
     val dexPanel: @Composable () -> Unit = { TextPanel("DEX", dexTextArea, explorerState) }
     val oatPanel: @Composable () -> Unit = { TextPanel("OAT", oatTextArea, explorerState) }
     var panels by remember { mutableStateOf(explorerState.getPanels(sourcePanel, dexPanel, oatPanel)) }
-
+    val onProgressUpdate: (String, Float) -> Unit = { newStatus: String, newProgress: Float ->
+        status = newStatus
+        progress = newProgress
+    }
+    
     MainMenu(
         explorerState,
         sourceTextArea,
@@ -137,7 +144,7 @@ private fun FrameWindowScope.KotlinExplorer(
             }
         },
         { oat -> updateTextArea(oatTextArea, oat) },
-        { statusUpdate -> status = statusUpdate },
+        onProgressUpdate,
         { findDialog.isVisible = true },
         { SearchEngine.find(activeTextArea, findDialog.searchContext) },
         { showSettings = true },
@@ -154,14 +161,17 @@ private fun FrameWindowScope.KotlinExplorer(
             modifier = Modifier.background(JewelTheme.globalColors.paneBackground)
         ) {
             MultiSplitter(modifier = Modifier.weight(1.0f), panels)
-            Row {
+            Row(verticalAlignment = CenterVertically) {
+                val width = 160.dp
                 Text(
                     modifier = Modifier
-                        .weight(1.0f, true)
-                        .align(Alignment.CenterVertically)
+                        .widthIn(min = width, max = width)
                         .padding(8.dp),
                     text = status
                 )
+                if (progress < 1) {
+                    LinearProgressIndicator({ progress })
+                }
             }
         }
     }
@@ -253,7 +263,7 @@ private fun FrameWindowScope.MainMenu(
     sourceTextArea: RSyntaxTextArea,
     onDexUpdate: (String?) -> Unit,
     onOatUpdate: (String) -> Unit,
-    onStatusUpdate: (String) -> Unit,
+    onStatusUpdate: (String, Float) -> Unit,
     onFindClicked: () -> Unit,
     onFindNextClicked: () -> Unit,
     onOpenSettings: () -> Unit,
