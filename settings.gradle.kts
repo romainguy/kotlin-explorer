14f457bab72767f8214049bc81e41710de61866a
Alon Albert <aalbert@google.com>
2024-05-05 19:31:06 -0700

    Gradle Cleanup
    * Added a version catalog
    * Removed what seems to be unused dependencies
    * Updated all libs to latest.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,6 @@ pluginManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        kotlin("multiplatform").version(extra["kotlin.version"] as String)
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-    }
 }
 
 plugins {
