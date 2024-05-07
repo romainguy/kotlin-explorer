af8a3a20d1838c3ae957eb1ee0ee67411c3c9a8d
Romain Guy <romainguy@curious-creature.com>
2024-05-06 18:43:27 -0700

    Unused import

--- src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/SwingPanel.kt
+++ src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/SwingPanel.kt
@@ -20,7 +20,6 @@ package dev.romainguy.kotlin.explorer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
