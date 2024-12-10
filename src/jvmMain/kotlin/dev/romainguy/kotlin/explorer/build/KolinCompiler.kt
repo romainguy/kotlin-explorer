53e8ba1fe39fb29e01354f01a55fbdd12066b2b6
Clayton Green <clayton.green@protonmail.com>
2024-12-09 18:02:07 -0800

    Use `File.pathSeparator` to separate `kotlinc -classpath` args

--- src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/build/KolinCompiler.kt
+++ src/jvmMain/kotlin/dev/romainguy/kotlin/explorer/build/KolinCompiler.kt
@@ -17,8 +17,8 @@
 package dev.romainguy.kotlin.explorer.build
 
 import dev.romainguy.kotlin.explorer.ToolPaths
-import dev.romainguy.kotlin.explorer.isWindows
 import dev.romainguy.kotlin.explorer.process
+import java.io.File
 import java.nio.file.Path
 
 private val BuiltInFiles = listOf(
@@ -35,7 +35,7 @@ class KotlinCompiler(private val toolPaths: ToolPaths, private val outputDirecto
             toolPaths.kotlinc.toString(),
             "-Xmulti-platform",
             "-classpath",
-            (toolPaths.kotlinLibs + listOf(toolPaths.platform)).joinToString(if (isWindows) ";" else ":") { jar -> jar.toString() }
+            (toolPaths.kotlinLibs + listOf(toolPaths.platform)).joinToString(File.pathSeparator) { jar -> jar.toString() }
         ).apply {
             if (kotlinOnlyConsumers) {
                 this += "-Xno-param-assertions"
